5d0cbaf46b3d85759ad29316f553c240518cf264
M-Kartheeswari <113534851+M-Kartheeswari@users.noreply.github.com>
2022-10-16 12:45:59 +0530

    Add files via upload

---
+++ InsertApp.java
@@ -0,0 +1,51 @@
+package net.sqlitetutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class InsertApp {
+
+    
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO movies(name,actor,actress,director,year_of_release) VALUES(?,?,?,?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+	    pstmt.setString(2, actor);
+	    pstmt.setString(3, actress);
+	    pstmt.setString(4, director);
+            pstmt.setInteger(5, year_of_release);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    
+    public static void main(String[] args) {
+
+        InsertApp app = new InsertApp();
+        
+        app.insert("Sarkar","Vijay","Keerthisuresh","A.R.Murugadoss", 2018);
+        app.insert("Valimai","Ajithkumar","HumaQureshi","H.Vinoth",2022);
+        app.insert("Cobra","Vikram","SrinidhiShetty","R.AjayGnanamuthu",2022);
+    }
+
+}
